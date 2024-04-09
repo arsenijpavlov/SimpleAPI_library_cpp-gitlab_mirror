b694ebe96a12b5ab7f8f2b3f46c60bf0fdb97b76
Riddick <arsenijpavlov@gmail.com>
2024-04-09 17:45:17 +0300

    Add parameters checker for Client

--- Client/main.cpp
+++ Client/main.cpp
@@ -2,8 +2,49 @@
 
 #include <iostream>
 
+bool read = false;
+std::string readFilePath;
+bool write = false;
+std::string writeFilePath;
+
+void ParseParameter(std::string parameter) {
+    if(read) {
+        readFilePath = parameter;
+        read = false;
+        return;
+    }
+    if(write) {
+        writeFilePath = parameter;
+        write = false;
+        return;
+    }
+
+    if (parameter == "read") {
+        read = true;
+    } else if (parameter == "write") {
+        write = true;
+    }
+}
+
 int main(int argc, char **argv) {
     using namespace json;
+
+    //parse arguments
+    for(int _argc = 1; _argc < argc && argc > 0; _argc++) {
+//        std::cout << "argc[" << _argc << "]: " << argv[_argc] << std::endl;
+        ParseParameter(argv[_argc]);
+    }
+
+    bool ret;
+    if(!readFilePath.empty()) {
+        Json json;
+        ret = json.readFile(readFilePath);
+        std::cout << "File is read: " << (ret ? "true" : "false") << std::endl;
+        std::cout << std::endl;
+        std::cout << "Json \"" << readFilePath << "\":" << std::endl;
+        std::cout << json.to_string(-1) << std::endl;
+    }
+
 //    Json json, json2;
 //    json.put("TestString", "TestValue");
 //    json.put("TestNum", "999");
@@ -11,7 +52,6 @@ int main(int argc, char **argv) {
 //    json2.put("Json2_2", "2");
 //    json.put("input_json", json2);
 
-    bool ret;
 //    ret = json.writeFile("./test.json");
 //    std::cout << "File is written: " << (ret ? "true" : "false") << std::endl;
 
@@ -19,12 +59,6 @@ int main(int argc, char **argv) {
 //    std::cout << json.to_string() << std::endl;
 //    std::cout << std::endl << std::endl;
 
-    Json json3;
-    ret = json3.readFile("./test.json");
-    std::cout << "File is read: " << (ret ? "true" : "false") << std::endl;
-    std::cout << std::endl;
-    std::cout << "Json3:" << std::endl;
-    std::cout << json3.to_string(-1) << std::endl;
 
     return 0;
 }
