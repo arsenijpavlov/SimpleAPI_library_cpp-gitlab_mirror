f0b6089323945e0d4c5718bfccb06958900c5a91
Riddick <arsenijpavlov@gmail.com>
2024-06-29 13:19:03 +0300

    Class creator script done

--- create_new_class.sh
+++ create_new_class.sh
@@ -4,11 +4,26 @@ if [[ ( -z $1 ) || (-z $(realpath $1) ) ]]; then
 	echo "incorrect <path> parameter"
 	exit 1
 fi
-echo -n "in path [$1] create "
+path=$1
+path="${path%%\/}"
+echo -n "in path [$path] create "
 
 if [[ -z $2 ]]; then
 	echo "incorrect <file_name> parameter"
 	exit 1
 fi
+echo "[$2] class file"
 
-echo "" > "$1/$2.h"
+echo "#ifndef ${2^^}_H" > "$path/$2.h"
+echo "#define ${2^^}_H" >> "$path/$2.h"
+echo "" >> "$path/$2.h"
+echo "" >> "$path/$2.h"
+echo "class $2 {" >> "$path/$2.h"
+echo "" >> "$path/$2.h"
+echo "}" >> "$path/$2.h"
+echo "" >> "$path/$2.h"
+echo "#endif // ${2^^}_H" >> "$path/$2.h"
+
+echo "#include \"$2.h\"" > "$path/$2.cpp"
+echo "" >> "$path/$2.cpp"
+echo "" >> "$path/$2.cpp"
