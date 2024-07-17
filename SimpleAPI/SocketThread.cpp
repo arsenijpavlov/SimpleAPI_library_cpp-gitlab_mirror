1e5e1ed64bf5146104d8225072608ef1988cd6ad
Riddick <arsenijpavlov@gmail.com>
2024-07-17 13:14:42 +0300

    disable redundancy logs

--- SimpleAPI/SocketThread.cpp
+++ SimpleAPI/SocketThread.cpp
@@ -7,21 +7,21 @@ void SocketThread::run() {
     pthread_setname_np(pthread_self(), "SERVERS_THREAD");
 
     while(this->isActive()) {
-        if(!this->isActive())
-            std::cout << "(M) not active" << std::endl;
+//        if(!this->isActive())
+//            std::cout << "(M) not active" << std::endl;
 
         for(auto it = this->p_sockets.begin(); it != this->p_sockets.end(); it++) {
             Socket* sock = it->second.get();
 
             sock->tick(); //вся магия там
-            if(!this->isActive())
-                std::cout << "not active" << std::endl;
+//            if(!this->isActive())
+//                std::cout << "not active" << std::endl;
         }
 
         usleep(1);
     }
 
-    std::cout << "[THREAD STOPED]" << std::endl;
+    std::cout << "[THREAD STOPPED]" << std::endl;
 }
 
 SocketThread::SocketThread() :
