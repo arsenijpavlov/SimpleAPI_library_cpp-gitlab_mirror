6bea70645e44fed45f271cf399f157c70c1f9b4d
Riddick <arsenijpavlov@gmail.com>
2024-08-08 19:03:08 +0300

    processing...

--- SimpleAPI/JsonSettings.h
+++ SimpleAPI/JsonSettings.h
@@ -5,18 +5,53 @@
 
 #include <string>
 
-class JsonSettings {
-    Json m_settings_without_groups; //по задумке, они должны идти ДО объявленных в группе
-    Json m_settings_in_groups;
-    Json m_comments;
+/* Стиль комментариев в файле:
+ *
+ * # go;ijnh;bofdj;ov
+ *
+ * или
+ *
+ * ###################################
+ * # gindooldgbmnd
+ * # dfsnmjfpsdkjfd's[ok ['d
+ * #
+ * # dfsdm'gopsk'[
+ * ###################################
+ *
+ */
 
-    std::vector<std::string> m_current_group;
-    std::string m_one_comment;
-    std::string m_start_multi_line_comment;
-    std::string m_end_multi_line_comment;
+enum class CommentType {
+    eBeforeValue,           // пользователь сам распределяет переносы строки
+    eBeforeValueMultiLine,  // автоматическая расстановка переносов строки
+    eAfterValueOneLine      // подсказка на той же строке
+};
+
+/* группа значений
+ *  - комментарий
+ *  - Json для хранения значений и комментариев к ним
+ *
+*/
+class SettingsGroup {
 
 public:
-    JsonSettings(){
+    SettingsGroup(const std::string& name) {}
+};
+
+/* основной класс настроек
+ *
+*/
+class Settings {
+    Json m_settings;            //по задумке, они должны идти ДО объявленных в группе
+    Json m_settings_in_groups;
+    Json m_comments;
+    Json m_comments_in_groups;
+
+    std::vector<std::string> m_current_group;
+    char m_comment_sym; //многострочные комментарии всегда имеют этот символ в начале строки
+    uint8_t m_comment_column_size;
+
+public:
+    Settings() : m_comment_column_size(60) {
         m_current_group.reserve(5); //преаллокация для 5 уровней вложенности
     }
 
@@ -25,9 +60,14 @@ public:
 
     void        beginGroup(const std::string& name);
     void        endGroup();
+
     void        setOrUpdate(const std::string& key, const Element& value,
-                            const std::string& comment = "", const bool use_auto_new_liner = false);
+                            const std::string& comment = "",
+                     const CommentType comment_type = CommentType::eBeforeValue);
     Element&    getOrDefault(const std::string& key, const Element& default_value);
+
+
+    void        setColumnWidth(const uint8_t new_width = 60);
 
 };
 
--- tests/test_json_settings.cpp
+++ tests/test_json_settings.cpp
@@ -14,17 +14,41 @@ int main(int argc, char **argv)
 //========================================================================================
 
 TEST(JSON_SETTINGS, write_to_file) {
-    JsonSettings json_settings;
-    json_settings.setOrUpdate("key_no_groupped", "value", "azaza");
-    json_settings.getOrDefault("key_no_groupped", "000");
-    json_settings.beginGroup("my first group");
-    {
-        std::string key;
-        std::string value;
-        std::string comment;
-        json_settings.setOrUpdate("key_no_groupped", "value", "azaza");
+    Settings settings;
+    //пользователь задаёт перевод строки каждые 10 символов комментария
+    //(без учёта начала комментария и пробела)
+    settings.setColumnWidth(10);
 
+    std::string comment = "eat some more of these soft French rolls and drink some tea";
+    settings.setOrUpdate("key_no_groupped", "value", comment);
+    settings.getOrDefault("key_no_groupped", "000");
+
+
+    //работа с группой значений
+    SettingsGroup settingsGroup("group_1");
+    settingsGroup.put("comment_key", comment);
+    settingsGroup.setOrUpdate("key_0", "value");
+    settingsGroup.setOrUpdate("key_1", "value", comment);
+    settingsGroup.setOrUpdate("key_2", "value", comment,
+                         CommentType::eBeforeValueMultiLine);
+    settingsGroup.setOrUpdate("key_in_group_3", "value", comment,
+                         CommentType::eAfterValueOneLine);
+
+    settings.setOrUpdate("group_name_1", inner_json);
+    JArray inner_array;
+    settings.setOrUpdate("group_name_2", inner_array);
+
+
+    //такая запись эквивалентна вложению Json или Array
+    settings.beginGroup("my first group");
+    {
+        settings.setOrUpdate("key_in_group_0", "value");
+        settings.setOrUpdate("key_in_group_1", "value", comment);
+        settings.setOrUpdate("key_in_group_2", "value", comment,
+                             CommentType::eBeforeValueMultiLine);
+        settings.setOrUpdate("key_in_group_3", "value", comment,
+                             CommentType::eAfterValueOneLine);
     }
-    json_settings.endGroup();
+    settings.endGroup();
 
 }
