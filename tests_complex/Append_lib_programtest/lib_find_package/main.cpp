3ce07a9e593fab44040f7aeda6f7ac9a559dfd55
Riddick <arsenijpavlov@gmail.com>
2025-03-12 02:08:11 +0300

    add test for include SimpleAPI as dynamic library

--- tests_complex/Append_lib_programtest/lib_find_package/main.cpp
+++ tests_complex/Append_lib_programtest/lib_find_package/main.cpp
@@ -9,6 +9,6 @@
 
 int main() {
     using namespace logs;
-    std::cout << to_color_string({COLOR::eBRIGHT_GREEN_FG, COLOR::eBOLD_TEXT}, "Hello world! Usage SimpleAPI!") << std::endl;
+    std::cout << to_color_string({COLOR::eBRIGHT_GREEN_FG, COLOR::eBOLD_TEXT}, "Hello world! Usage SimpleAPI as CMake::find_package() usage!") << std::endl;
     return 0;
 }
