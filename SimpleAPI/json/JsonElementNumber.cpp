50c91dec0ca407ce2f9e25481aee8277cd61d5d5
Riddick <arsenijpavlov@gmail.com>
2025-03-30 01:47:39 +0300

    done JsonElementNumber as long double

--- SimpleAPI/json/JsonElementNumber.cpp
+++ SimpleAPI/json/JsonElementNumber.cpp
@@ -1,3 +1,35 @@
 #include "JsonElementNumber.h"
 
+#include <regex>
 
+
+std::string ElementNumber::to_string(const ConfigFormat format,
+                                     const bool to_one_line) const noexcept {
+    return std::to_string(m_value);
+}
+
+bool ElementNumber::isEqual(const Element &other) const noexcept {
+    return m_value == dynamic_cast<const ElementNumber&>(other).m_value;
+}
+
+bool IsElementNumber(const std::string &str) noexcept {
+    if(str.empty()) return false;
+    if(str[0] == 'e' || str[0] == 'E' || str[0] == 'f' || str[0] == 'F')
+        return false;
+
+    std::regex reg("^[+-]?[0-9]*[.]?[0-9]*[eE]?[+-]?[0-9]*[fF]?$");
+    bool matched = std::regex_match(str, reg);
+
+    bool e_is_last = str[str.length() - 1] == 'e' || str[str.length() - 1] == 'E';
+    bool f_is_last = str[str.length() - 1] == 'f' || str[str.length() - 1] == 'F';
+
+    if(f_is_last && str.length() > 1) {
+        e_is_last = str[str.length() - 2] == 'e' || str[str.length() - 2] == 'E';
+    }
+
+    return matched && !e_is_last;
+}
+
+bool IsElementNumber(const Element &e) noexcept {
+    return e.getType() == ValueType::eNumber;
+}
--- SimpleAPI/json/JsonElementNumber.h
+++ SimpleAPI/json/JsonElementNumber.h
@@ -4,10 +4,38 @@
 #include "JsonBaseElement.h"
 
 
-//TODO: class ElementNumber
-class ElementNumber : Element {
-private:
+class ElementNumber : public Element {
+protected:
+    long double m_value;
+
 public:
+    ElementNumber() noexcept : m_value(0)       { m_type = ValueType::eNumber; }
+    ElementNumber(const long double& num) noexcept : m_value(num)
+                                                { m_type = ValueType::eNumber; }
+//TODO: фича под вопросом    ElementNumber(const std::string& num) noexcept
+    ~ElementNumber() noexcept;
+
+    //PRINTING =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    std::string to_string(const ConfigFormat format = ConfigFormat::eJSON,
+                          const bool to_one_line = false) const noexcept override;
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= PRINTING
+
+    long double getValue() const noexcept       { return m_value; }
+//TODO: фича под вопросом    int64_t     toInt() const noexcept          { return m_value; }
+//TODO: фича под вопросом    uint64_t    toUInt() const noexcept         { return m_value; }
+    bool        isEqual(const Element& other) const noexcept override;
+
+    //OPERATORS -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    void operator=(const long double& other)    { m_value = other; }
+    bool operator==(const long double& other)   { return m_value == other; }
+    bool operator>(const long double& other)    { return m_value > other; }
+    bool operator<(const long double& other)    { return m_value < other; }
+    bool operator>=(const long double& other)   { return m_value >= other; }
+    bool operator<=(const long double& other)   { return m_value <= other; }
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= OPERATORS
 };
+
+bool IsElementNumber(const std::string& str) noexcept;
+bool IsElementNumber(const Element& e) noexcept;
 
 #endif // JSON_ELEMENT_NUMBER_H
