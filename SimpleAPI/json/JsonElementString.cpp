16790567d9b6dfa6e9f52085a4c630c4890b3ac5
Riddick <arsenijpavlov@gmail.com>
2025-03-30 01:10:42 +0300

    done JsonElementString

--- SimpleAPI/json/JsonElementString.cpp
+++ SimpleAPI/json/JsonElementString.cpp
@@ -1,3 +1,101 @@
 #include "JsonElementString.h"
+#include "JsonDefines.h"
+
+#include "../utils/Utils.h"
 
 
+std::string ElementString::to_string(const ConfigFormat format, const bool to_one_line) const noexcept {
+    return m_value;
+}
+
+bool ElementString::isEqual(const Element &other) const noexcept {
+    return m_value == dynamic_cast<const ElementString&>(other).m_value;
+}
+
+void ElementString::operator<<(const std::string &other) noexcept {
+    m_value.append(other);
+}
+
+void ElementString::operator=(const std::string &other) noexcept {
+    m_value = other;
+}
+
+
+bool IsElementString(std::string &str, const ConfigFormat format,
+                     std::string* error_log) noexcept {
+    //удалить пробелы в начале и конце строки
+    RemoveIllegalSpaces(str);
+
+    bool done   = false;
+    char ch     = 0;
+    std::string temp;
+
+    switch(format) {
+    case ConfigFormat::eJSON: {
+        if(str[0] == '"') {
+            for(size_t i = 0; i < str.length(); i++) {
+                if(ch != 0) { //начинаем запись слова
+                    if(!done) {
+                        //экранированные символы ВСЕГДА заносятся в значение (все)
+                        if(str[i] == '\\') {
+                            if(str.length() > i + 1) {
+                                temp += str[i];
+                                temp += str[i+1];
+                                i++;
+                                continue;
+                            } else {
+                                if(error_log) *error_log = std::string("Error with parse String in: ") + str[i];
+                                return false;
+                            }
+                        }
+
+                        if(str[i] == '"')   done = true;
+                        else                temp += str[i];
+                    } else { //замкнули слово, надо проверить оставшиеся символы
+                        //TODO: пробелов уже быть не может! Надо сразу ошибку возвращать!
+                        if(!utils::CharsInString(str[i], __SPACES__)) {
+                            if(error_log) *error_log = std::string("Error with parse String in: ") + str[i];
+                            return false;
+                        }
+                    }
+                } else if(str[i] == '"') {
+                    ch = str[i];
+                }
+            }
+            str = temp;
+            return done;
+        } else {
+            for(char ch : str)
+                //TODO: пробелов уже быть не может! Надо сразу ошибку возвращать!
+                if(utils::CharsInString(ch, __SPACES__))
+                    return false;
+            return true;
+        }
+    }
+    case ConfigFormat::eINI: {
+        //TODO: подумать, что делать с переносами строк
+
+        for(size_t i = 0; i < str.length(); i++) {
+            //экранированные символы ВСЕГДА заносятся в значение (все)
+            if(str[i] == '\\') {
+                if(str.length() > i + 1) {
+                    temp += str[i] + str[i];
+                    i++;
+                    continue;
+                } else {
+                    if(error_log) *error_log = std::string("Error with parse String in: ") + str[i];
+                    return false;
+                }
+            } else
+                temp += str[i];
+        }
+        str = temp;
+        return true;
+    }
+    default: return false;
+    }
+}
+
+bool IsElementString(const Element &e) noexcept {
+    return e.getType() == ValueType::eString;
+}
--- SimpleAPI/json/JsonElementString.h
+++ SimpleAPI/json/JsonElementString.h
@@ -4,10 +4,33 @@
 #include "JsonBaseElement.h"
 
 
-//TODO: class ElementString
-class ElementString : Element {
-private:
+class ElementString : public Element {
+protected:
+    std::string m_value;
+
 public:
+    ElementString() noexcept : m_value()                        { m_type = ValueType::eString; }
+    ElementString(const std::string& s) noexcept : m_value(s)   { m_type = ValueType::eString; }
+    ~ElementString() noexcept                                   {}
+
+    //PRINTING =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    std::string to_string(const ConfigFormat format = ConfigFormat::eJSON,
+                          const bool to_one_line = false) const noexcept override;
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= PRINTING
+
+    std::string getValue() const noexcept                       { return m_value; }
+    bool        isEqual(const Element& other) const noexcept override;
+
+    //OPERATORS -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    void operator<<(const std::string& other) noexcept;
+    void operator=(const std::string& other) noexcept;
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= OPERATORS
 };
+
+//не const, потому что предполагается использовать эту же переменную для возврата прочитанного значения
+bool IsElementString(std::string& str, const ConfigFormat format = ConfigFormat::eJSON,
+                     std::string* error_log = nullptr) noexcept;
+bool IsElementString(const Element& e) noexcept;
+
 
 #endif // JSON_ELEMENT_STRING_H
