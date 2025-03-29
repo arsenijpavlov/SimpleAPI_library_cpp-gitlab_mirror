209f2722165c56e6e690c35d47004bb28b983f5b
Riddick <arsenijpavlov@gmail.com>
2025-03-30 01:07:34 +0300

    done JsonElementBool

--- SimpleAPI/json/JsonElementBool.cpp
+++ SimpleAPI/json/JsonElementBool.cpp
@@ -1,3 +1,33 @@
 #include "JsonElementBool.h"
+#include <algorithm>
 
 
+
+std::string ElementBool::to_string(const ConfigFormat format, const bool to_one_line) const noexcept {
+    return m_value ? "true" : "false";
+}
+
+bool ElementBool::isEqual(const Element &other) const noexcept {
+    return m_value == dynamic_cast<const ElementBool&>(other).m_value;
+}
+
+void ElementBool::operator=(const bool other) noexcept {
+    m_value = other;
+}
+
+
+bool IsElementBool(const std::string &str) noexcept {
+    std::string temp = str;
+
+    //удалить пробелы в начале и конце строки
+    RemoveIllegalSpaces(temp);
+    std::transform(temp.begin(), temp.end(), temp.begin(),
+                   [](unsigned char c){ return std::tolower(c); });
+
+    if(temp == "true" || temp == "false") return true;
+    return false;
+}
+
+bool IsElementBool(const Element &e) noexcept {
+    return e.getType() == ValueType::eBool;
+}
--- SimpleAPI/json/JsonElementBool.h
+++ SimpleAPI/json/JsonElementBool.h
@@ -4,10 +4,29 @@
 #include "JsonBaseElement.h"
 
 
-//TODO: class ElementBool
-class ElementBool : Element {
-private:
+class ElementBool : public Element {
+protected:
+    bool m_value;
+
 public:
+    ElementBool() noexcept : m_value(false)         { m_type = ValueType::eBool; }
+    ElementBool(const bool b) noexcept : m_value(b) { m_type = ValueType::eBool; }
+    ~ElementBool() noexcept;
+
+    //PRINTING =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    std::string to_string(const ConfigFormat format = ConfigFormat::eJSON,
+                          const bool to_one_line = false) const noexcept override;
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= PRINTING
+
+    bool        getValue() const noexcept           { return m_value; }
+    bool        isEqual(const Element& other) const noexcept override;
+
+    //OPERATORS -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    void operator=(const bool other) noexcept;
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= OPERATORS
 };
+
+bool IsElementBool(const std::string& str) noexcept;
+bool IsElementBool(const Element& e) noexcept;
 
 #endif // JSON_ELEMENT_BOOL_H
