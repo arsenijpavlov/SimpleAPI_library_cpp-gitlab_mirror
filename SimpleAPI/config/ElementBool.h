91c02b109ff79ab2a5167a7fae6ec1acf81b6bb8
Riddick <arsenijpavlov@gmail.com>
2025-05-17 01:54:42 +0300

    upd plan for ElementBool

--- SimpleAPI/config/ElementBool.h
+++ SimpleAPI/config/ElementBool.h
@@ -9,32 +9,51 @@ protected:
     bool m_value;
 
 public:
-    ElementBool() noexcept : m_value(false)         { m_type = ValueType::eBool; }
-    ElementBool(const bool b) noexcept : m_value(b) { m_type = ValueType::eBool; }
-    ~ElementBool() noexcept {}
+    ElementBool()               noexcept : m_value(false)   {}
+    ElementBool(const bool b)   noexcept : m_value(b)       {}
+    ~ElementBool()              noexcept                    {}
 
-    //PRINTING =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    //для рекурсивного вызова, без комментариев, в одну строку
-    std::string toString(const ConfigFormat format = ConfigFormat::eJSON,
-                         const int8_t tabulation_level = 0) const noexcept override
-                                                    { return m_value ? "true" : "false"; }
-    //для рекурсивного вызова, с использованием комментариев
-    std::string toString(const ConfigFormat format, const CommentDesign &design,
-                         const int8_t tabulation_level = 0) const noexcept override
-                                                    { return m_value ? "true" : "false"; }
-    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= PRINTING
+    // Getters =========================================================================================================
+    bool&           getBool()                                           override    { return m_value; }
+    bool            getBool()                           const           override    { return m_value; }
+    long double&    getNumber()                                         override;
+    long double     getNumber()                         const           override;
+    std::string&    getString()                                         override;
+    std::string     getString()                         const           override;
 
-    bool        getValue() const noexcept           { return m_value; }
-    bool&       getValue() noexcept                 { return m_value; }
+    // вложенные контейнеры
+    Config&         get_front()                                         override;
+    Config          get_front()                         const           override;
+    Config&         get_at(const size_t index)                          override;
+    Config          get_at(const size_t index)          const           override;
+    Config&         get_at(const std::string& key)                      override;
+    Config          get_at(const std::string& key)      const           override;
+    Config&         get_back()                                          override;
+    Config          get_back()                          const           override;
+    // ========================================================================================================= Getters
 
-    bool        isEqual(const IElement& other, const bool compare_comments = false) const noexcept override;
+    // Info ============================================================================================================
+    bool            isEqual(const bool other)           const noexcept  override    { return m_value; }
+    size_t          size()                              const noexcept  override    { return 1; }
+    // ============================================================================================================ Info
 
-    //OPERATORS -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    void operator=(const bool other) noexcept;
-    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= OPERATORS
+    // Operators =======================================================================================================
+    //числа, контейнеры(размер), строки(длина в видимых символах)
+    Config&         operator[](const size_t index)      noexcept        override;
+    Config          operator[](const size_t index)      const noexcept  override;
+    Config&         operator[](const std::string& key)  noexcept        override;
+    Config          operator[](const std::string& key)  const noexcept  override;
+    // ======================================================================================================= Operators
+
+    // String ==========================================================================================================
+    //вывод без комментариев, "tabulation_level == -1" => запись в одну строку
+    std::string     toString(const ConfigFormat format = ConfigFormat::eJSON,
+                        const int8_t tabulation_level = 0, const CommentDesign &design = {})
+                                                        const noexcept  override;
+    // ========================================================================================================== String
 };
 
-bool IsElementBool(const std::string& str) noexcept;
-bool IsElementBool(const IElement& e) noexcept;
+bool IsElementBool(const std::string& str)              noexcept;
+bool IsElementBool(const IElement& e)                   noexcept;
 
 #endif // ELEMENT_BOOL_H
