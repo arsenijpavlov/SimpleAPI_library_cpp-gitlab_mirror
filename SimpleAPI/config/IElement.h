4ecfd44f81355e318979cdda7c71298c630f3f56
Riddick <arsenijpavlov@gmail.com>
2025-06-07 15:20:35 +0300

    done IElement methods plan

--- SimpleAPI/config/IElement.h
+++ SimpleAPI/config/IElement.h
@@ -25,66 +25,11 @@ protected:
 public:
     virtual ~IElement()                                         noexcept            {}
 
-    // Getters =========================================================================================================
-//    bool&           getBool();
-//    bool            getBool()                           const;
-//    long double&    getNumber();
-//    long double     getNumber()                         const;
-//    std::string&    getString();
-//    std::string     getString()                         const;
-
-    // вложенные контейнеры
-//    Config&         get_front();
-//    Config          get_front()                         const;
-//    Config&         get_at(const size_t index);
-//    Config          get_at(const size_t index)          const;
-//    Config&         get_at(const std::string& key);
-//    Config          get_at(const std::string& key)      const;
-//    Config&         get_back();
-//    Config          get_back()                          const;
-    // ========================================================================================================= Getters
-
-    // Info ============================================================================================================
-    ValueType         getType()                                 const noexcept      { return m_type; }
-    virtual bool      isContainer()                             const noexcept      { return false; }
-    virtual bool      isMapContainer()                          const noexcept      { return false; }
-//    bool            isEqual(const Config& other)                const noexcept      { }
-    bool              isEqual(const IElement& other, const bool compare_comments = false)
-                                                                const;
-//    bool            isEqual(const bool other)                   const noexcept      { }
-//    bool            isEqual(const long double& other)           const noexcept      { }
-//    bool            isEqual(const std::string& other)           const noexcept      { }
-    virtual size_t    size()                                    const noexcept      { return 0; }
-    // ============================================================================================================ Info
-
-    // Operators =======================================================================================================
-//    bool            operator==(const Config& other)     const noexcept      { }
-//    bool            operator==(const bool other)        const noexcept      { }
-//    bool            operator==(const long double& other)const noexcept      { }
-//    bool            operator==(const std::string& other)const noexcept      { }
-
-//    bool            operator!=(const Config& other)     const noexcept      { }
-//    bool            operator!=(const bool other)        const noexcept      { }
-//    bool            operator!=(const long double& other)const noexcept      { }
-//    bool            operator!=(const std::string& other)const noexcept      { }
-
-    //TODO: operator<<
-
-    //числа, контейнеры(размер), строки(длина в видимых символах)
-//    bool            operator>(const Config& other)      const noexcept      { }
-//    bool            operator>=(const Config& other)     const noexcept      { }
-//    bool            operator<(const Config& other)      const noexcept      { }
-//    bool            operator<=(const Config& other)     const noexcept      { }
-//    Config&         operator[](const size_t index)      noexcept;
-//    Config          operator[](const size_t index)      const noexcept;
-//    Config&         operator[](const std::string& key)  noexcept;
-//    Config          operator[](const std::string& key)  const noexcept;
-    // ======================================================================================================= Operators
-
+    //общее
     //COMMENTS =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     void            addComment(const Comment& content)          noexcept            { m_comment = content; }
     void            addComment(const std::string &content_before, const std::string &content_after)
-                                                                noexcept            { m_comment = Comment(content_before, content_after); }
+        noexcept            { m_comment = Comment(content_before, content_after); }
     void            addPrefixComment(const std::string& content) noexcept           { m_comment.setPrefix(content); }
     void            addSuffixComment(const std::string& content) noexcept           { m_comment.setSuffix(content); }
 
@@ -107,36 +52,71 @@ public:
     void            setCommentDesign(const CommentDesign& design)
                                                                 noexcept            { m_comment.setDesign(design); }
     void            clearCommentDesign()                        noexcept            { m_comment.clearDesign(); }
-    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= COMMENTS
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= COMMENT
+
+    //индивидуальное
+    // Setters =========================================================================================================
+        // заполнение строго через конструкторы или operator=
+    // ========================================================================================================= Setters
+
+    // Getters =========================================================================================================
+        // внутри у каждого потомка должна быть реализация getValue()
+    // ========================================================================================================= Getters
+
+    // Modify ==========================================================================================================
+    //NOTE: не путать с remove(), здесь просто сброс комментариев, сброс значения до:
+    virtual Config& clear()                                     noexcept            = 0;
+    // ========================================================================================================== Modify
+
+    // Info ============================================================================================================
+    virtual ValueType   getType()                               const noexcept          { return ValueType::eNull; }
+    virtual bool        isContainer()                           const noexcept          { return false; }
+    virtual bool        isMapContainer()                        const noexcept          { return false; }
+
+    bool                isEqual(const IElement& other, const bool compare_comments = false) const;
+
+    //числа, контейнеры(размер), строки(длина в видимых символах)
+    virtual size_t      size()                                  const noexcept          { return 0; }
+    // ============================================================================================================ Info
+
+    // Operators =======================================================================================================
+        // operator=() не реализован для потомков
+        // operator==() не реализован для потомков
+        // operator!=() не реализован для потомков
+        // operator>() не реализован для потомков
+        // operator>=() не реализован для потомков
+        // operator<() не реализован для потомков
+        // operator<=() не реализован для потомков
+    //именованные контейнеры (индивидуально для ElementMapContainer классов)
+    //    Config&         operator[](const std::string& key);
+    //    Config          operator[](const std::string& key)              const;
+    //    Config&         operator[](const VString& complex_key);
+    //    Config          operator[](const VString& complex_key)          const;
+    // ======================================================================================================= Operators
 
     // String ==========================================================================================================
     //вывод без комментариев, "tabulation_level == -1" => запись в одну строку
-    virtual std::string     toString(const ConfigFormat format = ConfigFormat::eONLY_VALUE,
-                                const int8_t tabulation_level = 0, const CommentDesign &design = {})
-                                                                const noexcept;
+    virtual std::string toString(const ConfigFormat format = ConfigFormat::eONLY_VALUE,
+                                 const int8_t tabulation_level = 0,
+                                 const CommentDesign &design = {})                  const noexcept = 0;
+    // NOTE: работа с std::cout реализована в Config
     // ========================================================================================================== String
 
+    // File ============================================================================================================
+        // работа с файлами и парсингом не предусмотрена для этого класса
+    // ============================================================================================================ File
+
+    // Parser ==========================================================================================================
+        // работа с файлами и парсингом не предусмотрена для этого класса
+    // ========================================================================================================== Parser
+
     // Iterators =======================================================================================================
-    // (!) Для foreach итераторы необходимо кастить вручную к нужному типу; за основу брать типы ниже
-//    /* (!) */ void*                 begin();
-//    /* (!) */ void*                 end();
-//    /* (!) */ void*                 cbegin()                    const;
-//    /* (!) */ void*                 cend()                      const;
-    // (!) Для foreach итераторы необходимо кастить вручную к нужному типу; за основу брать типы ниже
-    //Array
-//    VElement::iterator              array_begin();
-//    VElement::iterator              array_end();
-//    VElement::const_iterator        array_cbegin()              const;
-//    VElement::const_iterator        array_cend()                const;
-    //Json
-//    VPairElement::iterator          json_begin();
-//    VPairElement::iterator          json_end();
-//    VPairElement::const_iterator    json_cbegin()               const;
-//    VPairElement::const_iterator    json_cend()                 const;
+        //индивидуально для ElementContainer классов
     // ======================================================================================================= Iterators
 };
 
 //----------------------------------------------------------------------------------------------------------------------
+//TODO: добавить СЮДА Is<TYPE>(std::string) парсинги?
 //----------------------------------------------------------------------------------------------------------------------
 
 #endif // I_ELEMENT_H
