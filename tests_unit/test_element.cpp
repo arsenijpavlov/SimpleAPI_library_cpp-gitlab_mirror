dcd6dc0949dde4e21c0a649bcae78fa7556209b1
Riddick <arsenijpavlov@gmail.com>
2025-08-30 11:52:27 +0300

    cleanup code for test_element.cpp

--- tests_unit/test_element.cpp
+++ tests_unit/test_element.cpp
@@ -15,71 +15,81 @@ int main(int argc, char **argv)
 
 TEST(ELEMENT, create_num) {
     Config el_int(1);
-    Config el_double(1.1);
-    Config el_uint(11);
-    Config el_float(11.f);
-
     EXPECT_EQ(ValueType::eNumber,  el_int.getType());
+
+    Config el_double(1.1);
     EXPECT_EQ(ValueType::eNumber,  el_double.getType());
+
+    Config el_uint(11);
     EXPECT_EQ(ValueType::eNumber,  el_uint.getType());
+
+    Config el_float(11.f);
     EXPECT_EQ(ValueType::eNumber,  el_float.getType());
 }
 
 TEST(ELEMENT, create_string) {
     Config el_string(std::string("asd"));
+    EXPECT_EQ(ValueType::eString, el_string.getType());
+
     Config el_char_arr("asd");
+    EXPECT_EQ(ValueType::eString, el_char_arr.getType());
+
     const char* chr = "asd";
     Config el_char_star(chr);
-
-    EXPECT_EQ(ValueType::eString, el_string.getType());
-    EXPECT_EQ(ValueType::eString, el_char_arr.getType());
     EXPECT_EQ(ValueType::eString, el_char_star.getType());
 }
 
 TEST(ELEMENT, create_bool) {
     Config el_bool(true);
-
     EXPECT_EQ(ValueType::eBool, el_bool.getType());
 }
 
 TEST(ELEMENT, create_null) {
     Config el_null;
-
     EXPECT_EQ(ValueType::eNull,    el_null.getType());
 }
 
 TEST(ELEMENT, create_json) {
     Config js(ValueType::eJson);
-    Config el_json(ValueType::eJson, "json", js);
+    EXPECT_EQ(js.getType(), ValueType::eJson);
 
+    Config el_json(ValueType::eJson, "json", js);
+    ASSERT_EQ(el_json.getType(), ValueType::eJson);
+
+    ASSERT_FALSE(el_json.isEmpty());
     EXPECT_EQ(ValueType::eJson, el_json.get_front().getType());
 }
 
 TEST(ELEMENT, create_array) {
     Config ar(ValueType::eArray);
+    EXPECT_EQ(ar.getType(), ValueType::eArray);
+
     Config el_array(ar);
+    ASSERT_EQ(el_array.getType(), ValueType::eArray);
 
     EXPECT_EQ(ValueType::eArray, el_array.get_front().getType());
 }
 
 TEST(ELEMENT, compare_all_types) {
     Config el_num(15.0);
-    Config el_bool(true);
-    Config el_string("true");
-    Config el_json(ValueType::eJson);
-    Config el_array(ValueType::eArray);
-
     EXPECT_EQ(el_num.getType(),     ValueType::eNumber);
-    EXPECT_EQ(el_bool.getType(),    ValueType::eBool);
-    EXPECT_EQ(el_string.getType(),  ValueType::eString);
-    EXPECT_EQ(el_json.getType(),    ValueType::eJson);
-    EXPECT_EQ(el_array.getType(),   ValueType::eArray);
+    EXPECT_EQ(el_num,               15.0);
 
-    EXPECT_EQ(el_num,   15.0);
-    EXPECT_EQ(el_bool,  true);
-    EXPECT_EQ(el_string,"true");
-    EXPECT_EQ(el_json,  Config(ValueType::eJson));
-    EXPECT_EQ(el_array, Config(ValueType::eArray));
+    Config el_bool(true);
+    EXPECT_EQ(el_bool.getType(),    ValueType::eBool);
+    EXPECT_EQ(el_bool,              true);
+
+    Config el_string("true");
+    EXPECT_EQ(el_string.getType(),  ValueType::eString);
+    EXPECT_EQ(el_string,            "true");
+
+    Config el_json(ValueType::eJson);
+    EXPECT_EQ(el_json.getType(),    ValueType::eJson);
+    EXPECT_EQ(el_json,              Config(ValueType::eJson));
+
+    Config el_array(ValueType::eArray);
+    EXPECT_EQ(el_array.getType(),   ValueType::eArray);
+    EXPECT_EQ(el_array,             Config(ValueType::eArray));
 }
 
 //TEST(ELEMENT, not_compare_all_types) {
