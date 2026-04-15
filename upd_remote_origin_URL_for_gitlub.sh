87386db042608a2c663ee51bc51dea5d3e21c37e
Riddick <arsenijpavlov@gmail.com>
2026-04-15 14:19:50 +0300

    add hint for SSL connection

--- upd_remote_origin_URL_for_gitlub.sh
+++ upd_remote_origin_URL_for_gitlub.sh
@@ -5,6 +5,9 @@
 # используя данные с репозитория github.com/arsenijpavlov@gmail.com/rserver
 ##############################################################################
 
+# отключение проверки валидности шифрования (самодподписанный сертификат)
+# git config http.sslVerify false
+
 cd "$(dirname "$(realpath "$0")")"
 
 FILENAME="temp.html"
@@ -23,10 +26,11 @@ OLD_IP=$(echo ${REMOTE_URL} | grep -oE "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+")
 echo "NEW_IP: ${NEW_IP}"
 echo "OLD_IP: ${OLD_IP}"
 echo "OLD REMOTE_URL: ${REMOTE_URL}"
+exit
 
 if [[ ${OLD_IP} != ${NEW_IP} ]]; then
 	REMOTE_URL=$(echo ${REMOTE_URL} | sed "s/${OLD_IP}/${NEW_IP}/")
-	git remote set-url origin ${REMOTE_URL}
+	git remote set-url origin "${REMOTE_URL}"
 	echo "NEW REMOTE_URL: ${REMOTE_URL}"
 else
 	echo "REMOTE_URL did not changed"
