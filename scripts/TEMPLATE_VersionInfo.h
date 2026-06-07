2ff78f80cf659cd9a1433454319c3545cea5a48e
Riddick <arsenijpavlov@gmail.com>
2026-06-07 23:52:01 +0300

    add scripts/check_version_from_git.sh for info about version

---
+++ scripts/TEMPLATE_VersionInfo.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define SIMPLEAPI_VERSION        "v0.7.0-b1 (with uncommited changes)"
+#define SIMPLEAPI_VERSION_NAME   "Version control"
+#define SIMPLEAPI_VERSION_COMMIT "079626a"
+
---
+++ scripts/check_version_from_git.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Описание ===========================================
+# Скрипт скопирует файл-шаблон для VersionInfo.h для #
+# передачи параметров версии из информации от Git в  #
+# проект библиотеки как define                       #
+# ====================================================
+
+cd $(dirname $(realpath "${0}"))
+
+FILE_NAME="VersionInfo.h"
+cp -f "TEMPLATE_${FILE_NAME}" "${PWD}/../SimpleAPI/${FILE_NAME}"
+
+# предварительное заполнение переменных
+DEFAULT_VALUE="UNKNOWN(git/.git not found)"
+SIMPLE_VERSION="${DEFAULT_VALUE}"
+SIMPLE_VERSION_NAME="${DEFAULT_VALUE}"
+SIMPLE_VERSION_COMMIT="${DEFAULT_VALUE}"
+
+# проверка наличия Git в системе
+result=$(git --version) # прячем консольный вывод
+if [[ $? == 0 ]]; then
+	# поиск последнего тега по маске и подсчёт количества коммитов от тега до текущего
+	GIT_OUTPUT=$(git describe --tags --match="v[0-9a-zA-Z\(\).,-]*" --long)
+	
+	# разделение прошлого вывода на составляющие
+	read -r SIMPLEAPI_VERSION SIMPLEAPI_VERSION_NAME SIMPLEAPI_VERSION_COMMIT < <( echo ${GIT_OUTPUT} \
+		| sed -E "s/^(v[0-9.-]*)[_]*([_0-9a-zA-Z.,-]+)-([0-9]+)-g([0-9a-fA-F]+)/\1-b\3 \2 \4/" \
+		| sed "s/-b0//" )
+	SIMPLEAPI_VERSION_NAME=${SIMPLEAPI_VERSION_NAME//_/ }
+	
+	# проверка наличия изменений без фиксации
+	GIT_DIFF_OUTPUT=$(git diff --name-only)
+	if [[ ${GIT_DIFF_OUTPUT} ]]; then
+		# есть изменения, надо сделать заполнитель
+		GIT_DIFF_OUTPUT=" (with uncommited changes)"
+		SIMPLEAPI_VERSION="${SIMPLEAPI_VERSION}${GIT_DIFF_OUTPUT}"
+	fi
+
+	echo "SIMPLEAPI_VERSION:        \"${SIMPLEAPI_VERSION}\""
+	echo "SIMPLEAPI_VERSION_NAME:   \"${SIMPLEAPI_VERSION_NAME}\""
+	echo "SIMPLEAPI_VERSION_COMMIT: \"${SIMPLEAPI_VERSION_COMMIT}\""
+fi
+
+# применение как define в коде C/C++
+function UpdParameter { # $1 - переменная, $2 - новое значение
+	sed -i "s/^\(#define ${1} [[:space:]]*\).*/\1\"${2}\"/" "${PWD}/../SimpleAPI/${FILE_NAME}"
+}
+
+UpdParameter "SIMPLEAPI_VERSION"        "${SIMPLEAPI_VERSION}"
+UpdParameter "SIMPLEAPI_VERSION_NAME"   "${SIMPLEAPI_VERSION_NAME}"
+UpdParameter "SIMPLEAPI_VERSION_COMMIT" "${SIMPLEAPI_VERSION_COMMIT}"
+
