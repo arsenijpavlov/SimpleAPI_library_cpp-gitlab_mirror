1b311a552a2b120d5be6801e35bc36f9815059b7
Riddick <arsenijpavlov@gmail.com>
2026-06-10 23:02:54 +0300

    cleanup for project files

--- cmake/generate_version_info.cmake
+++ cmake/generate_version_info.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 3.5)
 message("------Start generating version info-----")
 
 execute_process(
-    COMMAND bash "${CMAKE_CURRENT_LIST_DIR}/scripts/check_version_from_git.sh"
+    COMMAND bash "${CMAKE_CURRENT_LIST_DIR}/../scripts/check_version_from_git.sh"
 )
 
 message("----------------- done -----------------")
